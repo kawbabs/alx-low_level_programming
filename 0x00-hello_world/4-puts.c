d806b98cdb164644c71a31b099c5afe71323d7f2
kawbabs <kawbabs2@gmail.com>
2022-06-30 08:15:59 -0700

    a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- print the phrase "programming islike building a multilingual language"
+ * Return (0)if xited properly, non zero otherwise
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
