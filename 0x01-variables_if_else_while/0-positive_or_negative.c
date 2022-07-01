3ed6f7a3bcd0dee3779a46c3d84fc2ef6cf209c6
kawbabs <kawbabs2@gmail.com>
2022-07-01 09:54:56 -0700

    l

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,12 @@
 /**
  * main -  assign a random number to the variable n
  * each time it is executed
- * Return: Always 0
+ * Return: Always 0 (suceess)
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
