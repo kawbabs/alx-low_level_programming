11591de53d29e05727c6ae2a6af07b6c5424dfbb
kawbabs <kawbabs2@gmail.com>
2022-07-05 14:10:24 -0700

    prints alphanet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include 'main.h'
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line
+ * Return: Always zero (sucess)
+ */
+
+void print_alphabet(void);
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z');
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
